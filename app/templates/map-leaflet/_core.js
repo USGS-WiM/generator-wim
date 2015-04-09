987387c4f399464feeee6f3e6ef6ff894dba4705
Marty <marsmith@usgs.gov>
2015-04-09 15:06:38 -0400

    got leaflet map to appear
    added a 100% height to body and html in main.css.
    Still works fine with esri template

--- app/templates/map-leaflet/_core.js
+++ app/templates/map-leaflet/_core.js
@@ -7,6 +7,86 @@ $( document ).ready(function() {
 
 	/* create map */
 	var map = L.map('mapDiv').setView([37.77, -122.45], 12);
-	L.esri.basemapLayer('NationalGeographic').addTo(map);
+	var layer = L.esri.basemapLayer('NationalGeographic').addTo(map);
+	var layerLabels;
+
+	$('#mapDiv').height($('body').height());
+	map.invalidateSize();
+	/* create map */
+
+	/* basemap controller */
+	function setBasemap(basemap) {
+	    if (layer) {
+	      map.removeLayer(layer);
+	    }
+	    layer = L.esri.basemapLayer(basemap);
+	    map.addLayer(layer);
+	    if (layerLabels) {
+	      map.removeLayer(layerLabels);
+	    }
+
+	    if (basemap === 'ShadedRelief' || basemap === 'Oceans' || basemap === 'Gray' || basemap === 'DarkGray' || basemap === 'Imagery' || basemap === 'Terrain') {
+
+	      layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+	      map.addLayer(layerLabels);
+	    }
+	}
+
+	$('.basemapBtn').on('click',function() {
+	  	var baseMap = this.id.replace('btn','');
+
+	  	// https://github.com/Esri/esri-leaflet/issues/504 submitted issue that esri-leaflet basemaps dont match esri jsapi
+
+	  	switch (baseMap) {
+		    case 'Streets': baseMap = 'Streets'; break;
+		    case 'Satellite': baseMap = 'Imagery'; break;
+		    case 'Hybrid': baseMap = 'n/a'; break;
+		    case 'Topo': baseMap = 'n/a'; break;
+		    case 'Terrain': baseMap = 'ShadedRelief'; break;
+		    case 'Gray': baseMap = 'Gray'; break;
+		    case 'OSM': baseMap = 'n/a'; break;
+		    case 'NatGeo': baseMap = 'NationalGeographic'; break;
+		    case 'NatlMap': baseMap = 'n/a'; break;
+		}
+
+		setBasemap(baseMap);
+
+	});
+  	/* basemap controller */
+
+	/* geocoder control */
+     function showModal() {
+        $('#geosearchModal').modal('show');
+    }
+    // Geosearch nav menu is selected
+    $('#geosearchNav').click(function(){
+        showModal();
+    });
+    $('#geosearchNav2').click(function(){
+        showModal();
+    });
+        
+	var searchControl = new L.esri.Geocoding.Controls.Geosearch();//.addTo(map);
+
+	var results = new L.LayerGroup().addTo(map);
+
+	searchControl.on('results', function(data){
+		results.clearLayers();
+		for (var i = data.results.length - 1; i >= 0; i--) {
+		  results.addLayer(L.marker(data.results[i].latlng));
+		}
+	});
+	/* geocoder control */
+
+	/* legend control */
+	$('#legendButtonNavBar, #legendButtonSidebar').on('click', function () {
+        $('#legend').toggle();
+        //return false;
+    });
+    $('#legendClose').on('click', function () {
+        $('#legend').hide();
+    });
+    /* legend control */
+
 
 });
