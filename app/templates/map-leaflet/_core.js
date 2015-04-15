9ad03a641e347c247a096183ef8422052f35cfdd
Marty <marsmith@usgs.gov>
2015-04-15 11:57:27 -0400

    got layers working for leaflet (somewhat)

--- app/templates/map-leaflet/_core.js
+++ app/templates/map-leaflet/_core.js
@@ -6,13 +6,128 @@ $( document ).ready(function() {
 	'use strict';
 
 	/* create map */
-	var map = L.map('mapDiv').setView([37.77, -122.45], 12);
+	var map = L.map('mapDiv').setView([39.833333, -98.583333], 4);
 	var layer = L.esri.basemapLayer('NationalGeographic').addTo(map);
 	var layerLabels;
+
+
+    var groupedOverlays = {};
+	var layerControl = L.control.groupedLayers(null, groupedOverlays, {collapsed: false});
+
+	//hack to put layer control in different div
+	layerControl._map = map;
+	var controlDiv = layerControl.onAdd(map);
+	$('#toggle').append(controlDiv);
 
 	$('#mapDiv').height($('body').height());
 	map.invalidateSize();
 	/* create map */
+
+	/* parse layers.js */
+	$.each(allLayers, function (i,v) {
+
+		//make sure not a heading
+		if (!(v.wimOptions.type == "heading" || v.wimOptions.type == "radioParent")) {
+
+			if (v.visibleLayers) {
+				var visibleLayers = v.visibleLayers
+			}
+			else var visibleLayers = '';
+
+			console.log('looping over alllayers object, currently: ', v.url, visibleLayers, i)
+
+			if (v.wimOptions.layerType == "agisFeature") {
+				
+
+				var featureLayer = L.esri.featureLayer(v.url).addTo(map);
+	            layerControl.addOverlay(featureLayer, i, "Feature Layers");
+
+			}
+
+			else if (v.wimOptions.layerType == "agisWMS") {
+				
+
+				var wmsLayer = L.tileLayer.wms(v.url, v.options).addTo(map);
+	            layerControl.addOverlay(wmsLayer, i, "WMS Layers");
+
+			}
+			else {
+
+
+
+				
+				addMapServerLegend(v.url, visibleLayers, i)
+				//var mapLayer = new L.esri.Layers.DynamicMapLayer(v.url, v.options).addTo(map);
+				//layerControl.addOverlay(mapLayer, i + '<img alt="Legend Swatch" src="/images/purple-pin.png" />', "Map Layers");
+
+			}
+		}
+		
+	});
+
+	//get visible and non visible layer lists
+	function addMapServerLegend(mapService, visibleLayers, groupName) {
+
+		
+
+	    //get layers REST endpoint for default visible layer info
+	    $.getJSON(mapService + '/layers?f=pjson', function (layersResponse) {
+
+	    	console.log('in addmapserverLegend: ', groupName, layersResponse);
+	        var mapServiceLayers = [];
+	        $.each(layersResponse.layers, function () {
+
+	            if (this.defaultVisibility == true) {
+	                mapServiceLayers.push([this.id, this.name, true]);
+	            }
+	            else {
+	                mapServiceLayers.push([this.id, this.name, false]);
+	            }
+	        });
+
+	        //get legend REST endpoint for swatch
+	        $.getJSON(mapService + '/legend?f=pjson', function (legendResponse) {
+
+	        	console.log(groupName, ' mapservicelayers: ',mapServiceLayers);
+
+	            //loop over default visible layers
+	            $.each(mapServiceLayers, function () {
+
+	            	//make sure layer is part of visiblelayers array of layers.js
+	            	if (visibleLayers.indexOf(this[0]) != -1) {
+
+	            		console.log(groupName, this[0], visibleLayers,'legend: ',legendResponse.layers[this[0]].legend.length )
+		                var feature = legendResponse.layers[this[0]].legend;
+
+		                //swatch title
+		                var swatch = '<span>' + this[1] + '</span></br>';
+
+		                //build legend html for categorized feautres
+		                if (feature.length > 1) {
+		                    $.each(feature, function () {
+		                        swatch += '<img alt="Legend Swatch" src="data:image/png;base64,' + this.imageData + '" /><span>' + this.label.replace('<', '').replace('>', '') + '</span></br>'
+		                    })
+		                }
+		                //single features
+		                else {
+		                    swatch = '<img alt="Legend Swatch" src="data:image/png;base64,' + feature[0].imageData + '" /><span>' + this[1] + '</span>';
+		                }
+
+		                //create layer and add to layer control 
+		                
+		                var singleLayer = new L.esri.dynamicMapLayer(mapService, { layers: [this[0]] });
+		                layerControl.addOverlay(singleLayer, swatch, '<span><strong>' + groupName + '</strong></span>');
+
+		                //if default visibility is true, turn on layer
+		                if (this[2]) {
+		                    singleLayer.addTo(map);
+		                }
+		            }
+	            }); //each visible layer
+	        }); //get legend json
+	    });  //get layers json
+	}
+	/* parse layers.js */
 
 	/* basemap controller */
 	function setBasemap(basemap) {
@@ -55,6 +170,7 @@ $( document ).ready(function() {
   	/* basemap controller */
 
 	/* geocoder control */
+/*
 	$('#geosearchNav').click(function(){
 		$.noConflict()
 		$('#geosearchModal').modal('show');
@@ -65,6 +181,7 @@ $( document ).ready(function() {
 	        "textboxPosition" : "user-defined"
 	    });
     });
+*/
 	/* geocoder control */
 
 	/* legend control */
