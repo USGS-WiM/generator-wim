64c1e4230ef20c9545883a81e2a8ac39148a7518
Marty <marsmith@usgs.gov>
2015-04-17 15:57:59 -0400

    leaflet layer control sorta working
    this needs to be cleaned and streamlined quite a bit and i have some
    ideas, just tired of it for now.
    
    Found that there are some issues with the ESRI legend, like the
    "wetlands" heading appears twice.  I'm having the same thing happening.
    Can probably program around it with some more work.
    
    But potentially there may just be too many options for ESRI dynamic map
    server legends to account for.

--- app/templates/map-leaflet/_core.js
+++ app/templates/map-leaflet/_core.js
@@ -3,6 +3,7 @@
 //main document ready function
 $( document ).ready(function() {
 
+	//for jshint
 	'use strict';
 
 	/* create map */
@@ -10,132 +11,270 @@ $( document ).ready(function() {
 	var layer = L.esri.basemapLayer('NationalGeographic').addTo(map);
 	var layerLabels;
 
-
-    var groupedOverlays = {};
-	var layerControl = L.control.groupedLayers(null, groupedOverlays, {collapsed: false});
-
-	//hack to put layer control in different div
-	layerControl._map = map;
-	var controlDiv = layerControl.onAdd(map);
-	$('#toggle').append(controlDiv);
-
 	$('#mapDiv').height($('body').height());
 	map.invalidateSize();
 	/* create map */
 
+	//getLocation();
+
 	/* parse layers.js */
-	var count = allLayers.length;
-	$.each(allLayers, function (i,v) {
 
-		//make sure not a heading
-		if (!(v.wimOptions.type === 'heading' || v.wimOptions.type === 'radioParent')) {
+	//create global layers lookup
+	var mapLayers = [];
 
-			var visibleLayers;
-			if (v.visibleLayers) {
-				visibleLayers = v.visibleLayers;
-			}
-			else {
-				visibleLayers = '';
-			}
+	$.each(allLayers, function (index,group) {
+		console.log('processing: ', group.groupHeading)
 
-			console.log('looping over alllayers object, currently: ', v.url, visibleLayers, i);
+		//sub-loop over layers within this groupType
+		$.each(group.layers, function (layerName,layerDetails) {
 
-			//ESRI feature layers
-			if (v.wimOptions.layerType === 'agisFeature') {	
-				var featureLayer = L.esri.featureLayer(v.url).addTo(map);
-	            layerControl.addOverlay(featureLayer, '<span style="font-weight:400;">' + i + '</span>', 'Feature Layers');
+			//check for exclusiveGroup for this layer
+			var exclusiveGroupName = '';
+			if (layerDetails.wimOptions.exclusiveGroupName) {
+				exclusiveGroupName = layerDetails.wimOptions.exclusiveGroupName;
 			}
 
-			//deal with WMS layers
-			else if (v.wimOptions.layerType === 'agisWMS') {
-				var wmsLayer = L.tileLayer.wms(v.url, v.options).addTo(map);
-	            layerControl.addOverlay(wmsLayer, '<span style="font-weight:400;">' + i + '</span>', 'WMS Layers');
+			if (layerDetails.wimOptions.layerType === 'agisFeature') {	
+				var layer = L.esri.featureLayer(layerDetails.url);
+				addLayer(group.groupHeading, group.showGroupHeading, layer, layerName, exclusiveGroupName);
+				addMapServerLegend(layerName, layerDetails);
 			}
 
-			//assume we have a regular ESRI map server to add
-			else {
-
-				addMapServerLegend(v.url, visibleLayers, i);
-				//var mapLayer = new L.esri.Layers.DynamicMapLayer(v.url, v.options).addTo(map);
-				//layerControl.addOverlay(mapLayer, i + '<img alt='Legend Swatch' src='/images/purple-pin.png' />', 'Map Layers');
+			else if (layerDetails.wimOptions.layerType === 'agisWMS') {
+				var layer = L.tileLayer.wms(layerDetails.url, layerDetails.options).addTo(map);
+				addLayer(group.groupHeading, group.showGroupHeading, layer, layerName, exclusiveGroupName);
+				addMapServerLegend(layerName, layerDetails);
 			}
-		}
-		if (!--count) {
-			cloneLegend();
-		}
+
+			else if (layerDetails.wimOptions.layerType === 'agisDynamic') {
+				var layer = new L.esri.Layers.DynamicMapLayer(layerDetails.url, layerDetails.options).addTo(map);
+				addLayer(group.groupHeading, group.showGroupHeading, layer, layerName, exclusiveGroupName);
+				addMapServerLegend(layerName, layerDetails);
+			}		
+	    });  
 	});
 
-	function cloneLegend() {
-		//clone to legend
-		/*
-		$( ".leaflet-control-layers-group" ).each(function( index ) {
-		  	$('#legendDiv').append(this);
-		});
-		*/
-		var $legendControl = $('.leaflet-control-layers').clone();
-  		$('#legendDiv').html($legendControl);
+	function getLocation() {
 
+		$.ajax({
+			dataType: "jsonp",
+			url: "http://freegeoip.net/json/",
+			error: function (jqXHR, textStatus, errorThrown) {
+				console.log("IP Geolocation Error: ", textStatus);
+
+				//if timeout, do this
+				if (textStatus == 'timeout') {
+					//ZoomManager(0);
+				}
+			},
+			success: function (data) {
+				console.log("IP Geolocation Success: ", data);
+
+				//zoom to location
+				map.setView([data.latitude, data.longitude], 9)
+			},
+			timeout: 3000
+		});
+	}
+
+	function addLayer(groupHeading, showGroupHeading, layer, layerName, exclusiveGroupName) {
+
+		//add layer to map
+		layer.addTo(map);
+
+		//add layer to layer list
+		mapLayers.push([exclusiveGroupName,camelize(layerName),layer]);
+		
+		//check if its an exclusiveGroup item
+		if (exclusiveGroupName) {
+
+			//create radio button
+			var button = $('<input type="radio" name="' + camelize(exclusiveGroupName) + '" value="' + camelize(layerName) + '"checked>' + layerName + '</input></br>');
+
+			//click listener for radio button
+		    button.click(function(e) {
+
+		    	var newLayer = $(this).val();
+
+			    $.each(mapLayers, function () {
+
+			    	if (this[0] == exclusiveGroupName) {
+			    		if (this[1] == newLayer) {
+				    		console.log('adding layer: ',this[1]);
+				    		map.addLayer(this[2]);
+				    		$('#' + camelize(this[1])).toggle();
+				    	}
+				    	else {
+				    		if (map.hasLayer(this[2])) {
+				    			console.log('removing layer: ',this[1]);
+				    			map.removeLayer(this[2]);
+				    			//$('#' + camelize(this[1])).toggle();
+				    		}
+				    	}
+			    	}
+			    });
+			});
+		}
+
+		//not an eclusive group item
+		else {
+
+			//create layer toggle
+			var button = $('<div align="left" style="cursor: pointer;padding:5px;"><span class="glyphspan glyphicon glyphicon-ok"></span>&nbsp;&nbsp;' + layerName + '</div>');
+
+			//click listener for regular button
+		    button.click(function(e) {
+
+		    	//toggle checkmark
+		    	$(this).find('span.glyphspan').toggleClass('glyphicon glyphicon-ok');
+
+				e.preventDefault();
+		        e.stopPropagation();
+
+		        $('#' + camelize(layerName)).toggle();
+
+		        //layer toggle
+		        if (map.hasLayer(layer)) {
+		            map.removeLayer(layer);
+		        } else {
+		            map.addLayer(layer);
+		        }
+			});
+		}
+
+	    //group heading logic
+		if (showGroupHeading) {
+
+			//camelize it for divID
+			var groupDivID = camelize(groupHeading);
+
+			//check to see if this group already exists
+			if (!$('#' + groupDivID).length) {
+				//if it doesn't add the header
+				var groupDiv = $('<div id="' + groupDivID + '"><div class="alert alert-info" role="alert"><strong>' + groupHeading + '</strong></div></div>');
+				$('#toggle').append(groupDiv);
+			}
+
+			//if it does already exist, append to it
+			$('#' + groupDivID).append(button);
+		}
+
+		else {
+			//otherwise append
+			$('#toggle').append(button);
+		}	
+	}
+
+	function camelize(str) {
+	  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+	    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+	  }).replace(/\s+/g, '');
 	}
 
 	//get visible and non visible layer lists
-	function addMapServerLegend(mapService, visibleLayers, groupName) {
+	function addMapServerLegend(layerName, layerDetails) {
 
-	    //get layers REST endpoint for default visible layer info
-	    $.getJSON(mapService + '/layers?f=pjson', function (layersResponse) {
 
-	    	console.log('in addmapserverLegend: ', groupName, layersResponse);
-	        var mapServiceLayers = [];
-	        $.each(layersResponse.layers, function () {
+		if (layerDetails.wimOptions.layerType === 'agisFeature') {	
 
-	            if (this.defaultVisibility === true) {
-	                mapServiceLayers.push([this.id, this.name, true]);
-	            }
-	            else {
-	                mapServiceLayers.push([this.id, this.name, false]);
-	            }
-	        });
+			//for feature layer since default icon is used, put that in legend
+			var legendItem = $('<div align="left" id="' + camelize(layerName) + '"><img alt="Legend Swatch" src="https://raw.githubusercontent.com/Leaflet/Leaflet/master/dist/images/marker-icon.png" /><strong>&nbsp;&nbsp;' + layerName + '</strong></br></div>');
+			$('#legendDiv').append(legendItem);
+			
+		}
 
-	        //get legend REST endpoint for swatch
-	        $.getJSON(mapService + '/legend?f=pjson', function (legendResponse) {
+		else if (layerDetails.wimOptions.layerType === 'agisWMS') {
 
-	        	console.log(groupName, ' mapservicelayers: ',mapServiceLayers);
+			//for WMS layers, for now just add layer title
+			var legendItem = $('<div align="left" id="' + camelize(layerName) + '"><img alt="Legend Swatch" src="http://placehold.it/25x41" /><strong>&nbsp;&nbsp;' + layerName + '</strong></br></div>');
+			$('#legendDiv').append(legendItem);
 
-	            //loop over default visible layers
-	            $.each(mapServiceLayers, function () {
+		}
 
-	            	//make sure layer is part of visiblelayers array of layers.js
-	            	if (visibleLayers.indexOf(this[0]) !== -1) {
+		else if (layerDetails.wimOptions.layerType === 'agisDynamic') {
 
-	            		console.log(groupName, this[0], visibleLayers,'legend: ',legendResponse.layers[this[0]].legend.length );
-		                var feature = legendResponse.layers[this[0]].legend;
+			//create new legend div
+        	var legendItemDiv = $('<div align="left" id="' + camelize(layerName) + '"><strong>&nbsp;&nbsp;' + layerName + '</strong></br></div>');      
+        	$('#legendDiv').append(legendItemDiv);
 
-		                //swatch title
-		                var swatch = '<span style="font-weight:400;">' + this[1] + '</span></br>';
+			//get legend REST endpoint for swatch
+	        $.getJSON(layerDetails.url + '/legend?f=json', function (legendResponse) {
 
-		                //build legend html for categorized feautres
-		                if (feature.length > 1) {
-		                    $.each(feature, function () {
-		                        swatch += '<img alt="Legend Swatch" src="data:image/png;base64,' + this.imageData + '" /><small>' + this.label.replace('<', '').replace('>', '') + '</small></br>';
-		                    });
-		                }
-		                //single features
-		                else {
-		                    swatch = '<img alt="Legend Swatch" src="data:image/png;base64,' + feature[0].imageData + '" /><span >' + this[1] + '</span>';
-		                }
+	        	console.log(layerName,'legendResponse',legendResponse);
 
-		                //create layer and add to layer control 
-		                
-		                var singleLayer = new L.esri.dynamicMapLayer(mapService, { layers: [this[0]] });
-		                layerControl.addOverlay(singleLayer, swatch, '<span>' + groupName + '</span>');
+	        	
 
-		                //if default visibility is true, turn on layer
-		                if (this[2]) {
-		                    singleLayer.addTo(map);
-		                }
-		            }
-	            }); //each visible layer
+	        	//make list of layers for legend
+				if (layerDetails.options.layers) {
+					//console.log(layerName, 'has visisble layers property')
+					//if there is a layers option included, use that
+					var visibleLayers = layerDetails.options.layers;
+				}
+				else {
+					//console.log(layerName, 'no visible layers property',  legendResponse)
+					
+					//create visibleLayers array with everything
+					var visibleLayers = [];
+					$.grep(legendResponse.layers, function(i,v) {
+					  	visibleLayers.push(v);
+					});
+				}
+
+	            //loop over all map service layers
+	            $.each(legendResponse.layers, function (i, legendLayer) {
+
+	            	//var legendHeader = $('<strong>&nbsp;&nbsp;' + legendLayer.layerName + '</strong>');
+	            	//$('#' + camelize(layerName)).append(legendHeader);
+
+	            	//sub-loop over visible layers property
+	           		$.each(visibleLayers, function (i, visibleLayer) {
+
+	           			//console.log(layerName, 'visibleLayer',  visibleLayer);
+
+		        		if (visibleLayer == legendLayer.layerId) {
+
+		        			console.log(layerName, visibleLayer,legendLayer.layerId, legendLayer)
+
+		        			//console.log($('#' + camelize(layerName)).find('<strong>&nbsp;&nbsp;' + legendLayer.layerName + '</strong></br>'))
+
+		        			var legendHeader = $('<strong>&nbsp;&nbsp;' + legendLayer.layerName + '</strong></br>');
+	        				$('#' + camelize(layerName)).append(legendHeader);
+
+		        			//get legend object
+			                var feature = legendLayer.legend;
+			            /*
+			                //build legend html for categorized feautres
+			                if (feature.length > 1) {
+			             */
+
+			                	//placeholder icon
+			                	//<img alt="Legend Swatch" src="http://placehold.it/25x41" />
+
+			                    $.each(feature, function () {
+
+			                    	//make sure there is a legend swatch
+			                    	if (this.imageData) {
+				                        var legendFeature = $('<img alt="Legend Swatch" src="data:image/png;base64,' + this.imageData + '" /><small>' + this.label.replace('<', '').replace('>', '') + '</small></br>');
+
+				                        $('#' + camelize(layerName)).append(legendFeature);
+				                    }
+			                    });
+			             /*
+			                }
+			                //single features
+			                else {
+			                	var legendFeature = $('<img alt="Legend Swatch" src="data:image/png;base64,' + feature[0].imageData + '" /><small>&nbsp;&nbsp;' + legendLayer.layerName + '</small></br>');
+
+			                	//$('#legendDiv').append(legendItem);
+			                	$('#' + camelize(layerName)).append(legendFeature);
+		
+			                }
+			             */
+		        		}
+		            }); //each visible layer
+	            }); //each legend item
 	        }); //get legend json
-	    });  //get layers json
+		}	
 	}
 	/* parse layers.js */
 
